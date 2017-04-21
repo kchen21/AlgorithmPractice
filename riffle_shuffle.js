3bfc43f2c0498f47261354edcbf3d16767320e8c
Kenneth Chen <kennethchendbgt@gmail.com>
2017-04-21 15:41:30 -0400

    Write isSingleRiffleShuffleRecursiveOptimized

--- riffle_shuffle.js
+++ riffle_shuffle.js
@@ -21,3 +21,39 @@ const isSingleRiffleShuffleRecursive = (shuffledDeck, half1, half2) => {
     return false;
   }
 };
+
+// Note that our inputs are small and of constant size. Even though the function above could be more efficient,
+// it doesn't take up a lot of time and space at all. However, it's good for us to think about its time and space complexities.
+
+// It is important to note that the slice method takes up O(m) time and O(m) space, where m is the size of the array it creates.
+// We have n stack frames, and use slice twice in each frame except the last. We will count the time and space complexities of
+// the slice method being called on shuffleDeck, since the ones being called on half1 and half2 will just change the constant multiplier.
+// The first slice requires n - 1 time and n - 1 space, and we keep slicing until we get to 1 time and 1 space.
+// Thus, the total time and space required is: (n - 1) + (n - 2) + (n - 3) + ... + 1, resulting in O(n^2) time and O(n^2) space.
+
+// Solution 2: Instead of using slice, which creates new arrays, we can keep track of indices to save time and space.
+
+const isSingleRiffleShuffleRecursiveOptimized = (shuffledDeck, shuffledDeckIndex, half1, half1Index, half2, half2Index) => {
+  shuffledDeckIndex = shuffledDeckIndex || 0;
+  half1Index = half1Index || 0;
+  half2Index = half2Index || 0;
+
+  if (shuffledDeckIndex === shuffledDeck.length) {
+    return true;
+  }
+
+  if (shuffledDeck[shuffledDeckIndex] === half1[half1Index]) {
+    shuffledDeckIndex++;
+    half1Index++;
+  } else if (shuffledDeck[shuffledDeckIndex] === half2[half2Index]) {
+    shuffledDeckIndex++;
+    half2Index++;
+  } else {
+    return false;
+  }
+
+  return isSingleRiffleShuffleRecursiveOptimized(shuffledDeck, shuffledDeckIndex, half1, half1Index, half2, half2Index);
+};
+
+// We have n + 1 frames, so the space cost is still O(n).
+// However, for each frame, we have a constant number of operations, so the runtime is O(n) now instead of O(n^2).
